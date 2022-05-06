90f2f1608256129cf916283ed7d18800e3d5c101
GeeKIKU <siook1239@gmail.com>
2022-05-06 12:15:18 +0900

    Update Code

--- ShinChan_House_Control/ShinChan_House_Control.ino
+++ ShinChan_House_Control/ShinChan_House_Control.ino
@@ -1,18 +1,22 @@
 #include <Adafruit_NeoPixel.h>
 
 //Neopixel
-#define NEO_PIN 13 //네오픽셀 연결 번호
+#define NEO_PIN 12 //네오픽셀 연결 번호
 
-#define LEDNUM 33 //LED 개수
+#define LEDNUM 13 //LED 개수
 
 // G R B
 // 255 255 0
 // 255 99 71
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDNUM, NEO_PIN, NEO_GRB + NEO_KHZ800);
+int Led_Count = 0;
 
 void setup() {
   Serial.begin(9600);
+
+  pinMode(2, INPUT_PULLUP);
+  pinMode(LED_BUILTIN, OUTPUT);
 
   strip.begin();
   strip.show();
@@ -23,25 +27,58 @@ void setup() {
 
 void loop() {
   strip.clear();
-  NeopixelFunc();
+  
+  if(digitalRead(2) == 0) {
+    digitalWrite(LED_BUILTIN, 1);
+    if(Led_Count > 10) Led_Count = 0;
+    else Led_Count++;
+    delay(100);
+  }
+  
+  if(Led_Count == 1) NeopixelFunc(1, 0, 0, 0, 0, 0, 0);
+  else if(Led_Count == 2) NeopixelFunc(0, 1, 0, 0, 0, 0, 0);
+  else if(Led_Count == 3) NeopixelFunc(0, 0, 1, 0, 0, 0, 0);
+  else if(Led_Count == 4) NeopixelFunc(0, 0, 0, 1, 0, 0, 0);
+  else if(Led_Count == 5) NeopixelFunc(0, 0, 0, 0, 1, 0, 0);
+  else if(Led_Count == 6) NeopixelFunc(0, 0, 0, 0, 0, 1, 0);
+  else if(Led_Count == 7) NeopixelFunc(0, 0, 0, 0, 0, 0, 1);
+  else if(Led_Count == 8) NeopixelFunc(1, 1, 1, 1, 0, 0, 0);
+  else if(Led_Count == 9) NeopixelFunc(0, 0, 1, 1, 0, 0, 0);
+  else if(Led_Count == 10) NeopixelFunc(0, 0, 0, 1, 0, 0, 1);
   strip.show();
+  
   delay(10);
+  digitalWrite(LED_BUILTIN, 0);
 }
 
-void NeopixelFunc() {
+void NeopixelFunc(bool a, bool b, bool c, bool d, bool e, bool f, bool g) {
   
-  for (int i = 0; i < 2; i++) LightON(i);
-  for (int i = 2; i < 4; i++) LightON(i);
-  for (int i = 4; i < 6; i++) LightON(i);
-  for (int i = 6; i < 10; i++) LightON(i);
-  for (int i = 10; i < 33; i++) LightON(i);
-  
-}
-
-void LightON(int i) {
-  strip.setPixelColor(i, strip.Color(150, 255, 50));
-}
-
-void LightOFF(int i) {
-  strip.setPixelColor(i, strip.Color(0, 0, 0));
+  for (int i = 0; i < 2; i++) {
+    if(a) strip.setPixelColor(i, strip.Color(150, 255, 50)); 
+    else strip.setPixelColor(i, strip.Color(0, 0, 0)); 
+  } // 1층 화장실
+  for (int i = 2; i < 3; i++) {
+    if(b) strip.setPixelColor(i, strip.Color(150, 255, 50)); 
+    else strip.setPixelColor(i, strip.Color(0, 0, 0)); 
+  } // 1층 부엌 
+  for (int i = 3; i < 5; i++) {
+    if(c) strip.setPixelColor(i, strip.Color(150, 255, 50)); 
+    else strip.setPixelColor(i, strip.Color(0, 0, 0)); 
+  } // 1층 안방  
+  for (int i = 5; i < 8; i++) {
+    if(d) strip.setPixelColor(i, strip.Color(150, 255, 50)); 
+    else strip.setPixelColor(i, strip.Color(0, 0, 0)); 
+  } // 1층 거실 
+  for (int i = 8; i < 9; i++) {
+    if(e) strip.setPixelColor(i, strip.Color(150, 255, 50)); 
+    else strip.setPixelColor(i, strip.Color(0, 0, 0)); 
+  } // 1층 복도 
+  for (int i = 9; i < 12; i++) {
+    if(f) strip.setPixelColor(i, strip.Color(150, 255, 50)); 
+    else strip.setPixelColor(i, strip.Color(0, 0, 0)); 
+  } // 2층 창고 
+  for (int i = 12; i < 14; i++) {
+    if(g) strip.setPixelColor(i, strip.Color(150, 255, 50)); 
+    else strip.setPixelColor(i, strip.Color(0, 0, 0)); 
+  } // 2층 서재  
 }
